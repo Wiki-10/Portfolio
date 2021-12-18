e330e53f9c103a1358df9a3b57089510dfa9e580
Wiki-10 <alexhdzcontacto@gmail.com>
2021-12-17 19:42:41 -0600

    Added projects section

--- app/src/components/Hero/Hero.js
+++ app/src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br></br>
+        My Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of this portfolio is to showcase the projects i've made
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Contact me!
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
